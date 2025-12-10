12b4d0135a498ed895f7ae515953d08f24fa3452
kIwwwww1 <danil29070@gmail.com>
2025-12-10 14:05:12 +0300

    Создание докерфайла и json файла с данными

--- src/database/database.py
+++ src/database/database.py
@@ -15,7 +15,7 @@ DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DB_HO
 engine = create_async_engine(DATABASE_URL)
 new_session = async_sessionmaker(engine)
 
-
+# async_session
 async def get_session():
     async with new_session() as session:
         yield session
--- src/models/models.py
+++ src/models/models.py
@@ -1,7 +1,8 @@
-from typing import List, Annotated
+from typing import List
 from sqlalchemy import func, ForeignKey, Integer, DateTime, BigInteger
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
 from datetime import datetime
+from src.database.database import engine
 
 class Base(DeclarativeBase):
     pass
@@ -11,12 +12,9 @@ class Video(Base):
 
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+
     creator_id: Mapped[int] = mapped_column(Integer, index=True, nullable=False)
-
-
     video_created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
-
-
     views_count: Mapped[int] = mapped_column(BigInteger, nullable=False, default=0)
     likes_count: Mapped[int] = mapped_column(BigInteger, nullable=False, default=0)
     comments_count: Mapped[int] = mapped_column(BigInteger, nullable=False, default=0)
@@ -27,7 +25,6 @@ class Video(Base):
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now())
 
 
-    # relationship to snapshots
     snapshots: Mapped[List["VideoSnapshot"]] = relationship("VideoSnapshot", back_populates="video", cascade="all, delete-orphan")
 
 
@@ -36,19 +33,14 @@ class VideoSnapshot(Base):
 
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+
     video_id: Mapped[int] = mapped_column(Integer, ForeignKey("videos.id", ondelete="CASCADE"), index=True, nullable=False)
-
-
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False, index=True)
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
-
-
     views_count: Mapped[int] = mapped_column(BigInteger, nullable=False, default=0)
     likes_count: Mapped[int] = mapped_column(BigInteger, nullable=False, default=0)
     comments_count: Mapped[int] = mapped_column(BigInteger, nullable=False, default=0)
     reports_count: Mapped[int] = mapped_column(BigInteger, nullable=False, default=0)
-
-
     delta_views_count: Mapped[int] = mapped_column(BigInteger, nullable=False, default=0)
     delta_likes_count: Mapped[int] = mapped_column(BigInteger, nullable=False, default=0)
     delta_comments_count: Mapped[int] = mapped_column(BigInteger, nullable=False, default=0)
@@ -57,3 +49,10 @@ class VideoSnapshot(Base):
 
     video: Mapped[Video] = relationship("Video", back_populates="snapshots")
 
+
+async def create_database():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def inserd_test_data(file_name: str):
